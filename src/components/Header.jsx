42bf9326b80742364cfcf84ed7b76072596f5d74
Zenlac <caleleizengrillera@gmail.com>
2024-10-04 22:42:23 +0800

    just finished the header

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './images/Logo.png';
+function Header() {
+  return (
+<div className='text-white font-lato'>
+    <div className='flex fixed justify-center items-center w-screen h-[100px] mx-auto z-10 bg-[#1E3E62]'>
+        <img src={logo} alt="Logo" className='h-[90px] w-[90px] mt-1 ml-12'/>
+        <ul className='flex justify-center gap-8 text-[24px] text-center items-center m-auto list-inside'>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Shop</li>
+            <li>Menu</li>
+            <li>Contact Us</li>
+        </ul>
+        <button className='absolute-right px-4 py-2 bg-[#FF6500] mr-8 rounded-[12px] text-[20px] font-bold'>Reserve Now</button>
+
+    </div>
+</div>
+
+  )
+}
+
+export default Header
