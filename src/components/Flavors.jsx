e3eec31384b1d3f17a10944118234aa205d028cb
Eizen <caleleizengrillera@gmail.com>
2024-10-07 08:34:00 +0800

    fixed title and logo

--- src/components/Flavors.jsx
+++ src/components/Flavors.jsx
@@ -16,22 +16,22 @@ function Flavors() {
                 <div className='flex'>
                   <img src={scale} alt="scales" className='flex h-[100px] mt-4'/>
                   <div className=' w-[300px]'>
-                    <h5 className='font-bold text-[24px]'>Quality Ingredients</h5>
-                    <h6 className='font-semibold text-[20px]'>—rich, aromatic spices, fresh herbs, and nurturing staples that evoke the essence of tradition, hospitality, and vibrant flavors.</h6>
+                    <h5 className='font-lato font-bold text-[24px]'>Quality Ingredients</h5>
+                    <h6 className='font-lato font-semibold text-[20px]'>—rich, aromatic spices, fresh herbs, and nurturing staples that evoke the essence of tradition, hospitality, and vibrant flavors.</h6>
                   </div>  
                 </div>
                 <div className='flex ml-12'>
                   <img src={speed} alt="clock" className='flex h-[100px] mt-4 '/>
                   <div className=' w-[300px]'>
-                    <h5 className='font-bold text-[24px]'>Fast and Friendly</h5>
-                    <h6 className='font-semibold text-[20px]'>—where every interaction is infused with genuine hospitality, making you feel like part of the family from the moment you arrive.</h6>
+                    <h5 className='font-lato font-bold text-[24px]'>Fast and Friendly</h5>
+                    <h6 className='font-lato font-semibold text-[20px]'>—where every interaction is infused with genuine hospitality, making you feel like part of the family from the moment you arrive.</h6>
                   </div>  
                 </div>
                 <div className='flex ml-6'>
                   <img src={chaching} alt="clock" className='flex h-[100px] mt-4'/>
                   <div className=' w-[300px]'>
-                    <h5 className='font-bold text-[24px]'>Fast and Friendly</h5>
-                    <h6 className='font-semibold text-[20px]'>—where every interaction is infused with genuine hospitality, making you feel like part of the family from the moment you arrive.</h6>
+                    <h5 className='font-lato font-bold text-[24px]'>Fast and Friendly</h5>
+                    <h6 className='font-lato font-semibold text-[20px]'>—where every interaction is infused with genuine hospitality, making you feel like part of the family from the moment you arrive.</h6>
                   </div>  
                 </div>
             </div>
@@ -44,22 +44,22 @@ function Flavors() {
             <div className='flex ml-5'>
                   <img src={paper} alt="scales" className='flex h-[100px] mt-4 pr-8'/>
                   <div className=' w-[300px]'>
-                    <h5 className='font-bold text-[24px]'>Diverse Menu</h5>
-                    <h6 className='font-semibold text-[20px]'>—offering a vibrant array of flavors and textures that reflect the country’s rich cultural tapestry and culinary heritage.</h6>
+                    <h5 className='font-lato font-bold text-[24px]'>Diverse Menu</h5>
+                    <h6 className='font-lato font-semibold text-[20px]'>—offering a vibrant array of flavors and textures that reflect the country’s rich cultural tapestry and culinary heritage.</h6>
                   </div>  
                 </div>
                 <div className='flex ml-12'>
                   <img src={utensils} alt="clock" className='flex h-[100px] mt-4 opacity-75'/>
                   <div className=' w-[300px] '>
-                    <h5 className='font-bold text-[24px]'>Best Recipes</h5>
-                    <h6 className='font-semibold text-[20px]'>—where time-honored techniques and fresh ingredients come together to create comforting, soul-soothing dishes that celebrate tradition and bring people closer.</h6>
+                    <h5 className='font-lato font-bold text-[24px]'>Best Recipes</h5>
+                    <h6 className='font-lato font-semibold text-[20px]'>—where time-honored techniques and fresh ingredients come together to create comforting, soul-soothing dishes that celebrate tradition and bring people closer.</h6>
                   </div>  
                 </div>
                 <div className='flex ml-6'>
                   <img src={walis} alt="clock" className='flex h-[100px] mt-4 mr-6'/>
                   <div className=' w-[300px]'>
-                    <h5 className='font-bold text-[24px]'>A Must Cleanliness</h5>
-                    <h6 className='font-semibold text-[20px]'>—where every space is cared for with respect and attention, ensuring a welcoming, safe, and inviting atmosphere for all.</h6>
+                    <h5 className='font-lato font-bold text-[24px]'>A Must Cleanliness</h5>
+                    <h6 className='font-lato font-semibold text-[20px]'>—where every space is cared for with respect and attention, ensuring a welcoming, safe, and inviting atmosphere for all.</h6>
                   </div>  
                 </div>
             </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,13 @@ function Header() {
 <div className='text-white font-lato'>
     <div className='flex fixed justify-center items-center w-screen h-[100px] mx-auto z-10 bg-[#1E3E62]'>
         <img src={logo} alt="Logo" className='h-[90px] w-[90px] mt-1 ml-12'/>
+        <h3 className='text-[40px] font-play font-semibold'>Sadye's Restaurant</h3>
         <ul className='flex justify-center gap-8 text-[24px] text-center items-center m-auto list-inside'>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Shop</li>
-            <li>Menu</li>
-            <li>Contact Us</li>
+            <li><button>Home</button></li>
+            <li><button>About Us</button></li>
+            <li><button>Shop</button></li>
+            <li><button>Menu</button></li>
+            <li><button>Contact Us</button></li>
         </ul>
         <button className='absolute-right px-4 py-2 bg-[#FF6500] mr-8 rounded-[12px] text-[20px] font-bold'>Reserve Now</button>
 
